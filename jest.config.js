eb1a133939ccd8e2bde37377577e5040106aee20
shiyaam <shiyaamsunder@users.noreply.github.com>
2021-07-24 12:54:55 +0530

    ⚒ setup tests

---
+++ jest.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/coverage',
+    '<rootDir>/dist',
+  ],
+  moduleDirectories: [
+    '<rootDir>/node_modules',
+    '<rootDir>/components',
+    '<rootDir>/containers',
+    '<rootDir>/hooks',
+    '<rootDir>/pages',
+  ],
+  moduleNameMapper: {
+    '@hooks/(.*)': '<rootDir>/hooks/$1',
+    '@components(.*)': '<rootDir>/components/$1',
+    '@containers/(.*)': '<rootDir>/containers/$1',
+    '@pages/(.*)': '<rootDir>/pages/$1',
+    '@styles/(.*)': '<rootDir>/styles/$1',
+    '@test-utils': '<rootDir>/utils/test-utils',
+  },
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'components/**/*.{js,jsx,ts,tsx}',
+    'hooks/**/*.{js,jsx,ts,tsx}',
+    'containers/**/*.{js,jsx,ts,tsx}',
+    'pages/**/*.{js,jsx,ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+};
