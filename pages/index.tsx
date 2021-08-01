b4e67f6bb2fec77d358bc3efc83e05b021ed32f1
shiyaam <shiyaamsunder@users.noreply.github.com>
2021-08-01 17:14:09 +0530

    added meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,44 @@ import { Landing, ProductSection, SubscribeSection } from '../sections';
 export default function Home({ products }: { products: Category[] }) {
   return (
     <>
+      <Head>
+        <title>Ecommerce Next JS</title>
+        <meta name="title" content="Ecommerce Next JS" />
+        <meta
+          name="description"
+          content="Ecommerce website built using Next JS by Shiyaam Sunder."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://ecommerce-frontend-sigma.vercel.app/"
+        />
+        <meta property="og:title" content="Ecommerce Next JS" />
+        <meta
+          property="og:description"
+          content="Ecommerce website built using Next JS by Shiyaam Sunder."
+        />
+        <meta
+          property="og:image"
+          content="https://ecommerce-frontend-sigma.vercel.app/landing.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://ecommerce-frontend-sigma.vercel.app/"
+        />
+        <meta property="twitter:title" content="Ecommerce Next JS" />
+        <meta
+          property="twitter:description"
+          content="Ecommerce website built using Next JS by Shiyaam Sunder."
+        />
+        <meta
+          property="twitter:image"
+          content="https://ecommerce-frontend-sigma.vercel.app/landing.png"
+        />
+      </Head>
       <NavBar />
 
       <HomeWrapper>
